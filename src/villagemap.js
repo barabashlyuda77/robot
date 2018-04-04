d65c0ef86819a5b4971001a730be8a2ceb2534ce
lbarabash <barabashlyuda@gmail.com>
2018-04-04 18:51:25 +0200

    move roads configuration to external file

--- src/villagemap.js
+++ src/villagemap.js
@@ -1,13 +1,3 @@
-const roads = [
-  "Alice's House-Bob's House",   "Alice's House-Cabin",
-  "Alice's House-Post Office",   "Bob's House-Town Hall",
-  "Daria's House-Ernie's House", "Daria's House-Town Hall",
-  "Ernie's House-Grete's House", "Grete's House-Farm",
-  "Grete's House-Shop",          "Marketplace-Farm",
-  "Marketplace-Post Office",     "Marketplace-Shop",
-  "Marketplace-Town Hall",       "Shop-Town Hall"
-];
-
 function buildVillageMap(edges) {
   let map = Object.create(null);
   function addEdge(from, to) {
@@ -26,8 +16,4 @@ function buildVillageMap(edges) {
 
   return map;
 }
-
-const villageMap = buildVillageMap(roads);
-console.log(villageMap);
-
 module.exports = buildVillageMap;
