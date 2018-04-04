c05e0233067ca65549fbd0e8fb34c7122c6bd7f9
lbarabash <barabashlyuda@gmail.com>
2018-04-04 18:53:44 +0200

    improve testing for village map

--- test/villagemap.test.js
+++ test/villagemap.test.js
@@ -2,16 +2,39 @@ const { expect } = require('chai');
 const buildVillageMap = require('../src/villagemap.js');
 
 describe('buildVillageMap', () => {
-    const arr = ['A-B'];
+  context('when creating a map with one road', () => {
+    it('returns the map with two edges that connects the passed States', () => {
+        expect(buildVillageMap(
+          ['A-B']
+        )).to.deep.equal({
+          'A': ['B'],
+          'B': ['A']
+        });
+    });
+  });
 
-    const result = buildVillageMap(arr);
-
-    it('returns an object', () => {
-        expect(result).to.deep.equal({'A': ['B'], 'B': ['A']})
+  context('when creating a map with multiple roads', () => {
+    it('returns the map containing each State as key', () => {
+      expect(buildVillageMap(
+        ['A-B', 'D-C']
+      )).to.deep.equal({
+        'A': ['B'],
+        'B': ['A'],
+        'C': ['D'],
+        'D': ['C']
+      });
     });
 
-    it('returns an object with key and few vallues in', () => {
-        arr.push('A-C');
-        expect(buildVillageMap(arr)).to.deep.equal({'A': ['B', 'C'], 'B': ['A'], 'C': ['A']})
-    });
-})
+    context('when a State is in multiple roads', () => {
+      it('returns the map with multiple values for that State', () => {
+        expect(buildVillageMap(
+          ['A-B', 'A-C']
+        )).to.deep.equal({
+          'A': ['B', 'C'],
+          'B': ['A'],
+          'C': ['A']
+        });
+      });
+    })
+  });
+});
