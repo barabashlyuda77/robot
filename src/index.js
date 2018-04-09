e0ecbbe42ce24f73b694ea7aaa0d937dbfdcda96
lbarabash <barabashlyuda@gmail.com>
2018-04-09 11:01:32 +0200

    the iteration of random move was changed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { move } = require('./robot');
 
 const villageMap = buildVillageMap(roads);
 let randomLocation = _.sample(Object.keys(villageMap));
-for (let i = 0; i < roads.length; i++) {
+for (let i = 1; i <= roads.length; i++) {
   const from = randomLocation;
   randomLocation = move(villageMap, randomLocation);
   console.log(`#${i} = from ${from} to ${randomLocation}`);
