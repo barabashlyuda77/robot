26bcadefd3385e03cc77968818ca0231f4a4ad2e
lbarabash <barabashlyuda@gmail.com>
2018-04-18 15:44:15 +0200

    robot moves on tasks

--- src/robot.js
+++ src/robot.js
@@ -1,5 +1,21 @@
 const _ = require('lodash');
+const { areTasksToWorkOn, updateTasksStage, randomLocation } = require('./task');
 
-const move = (map, currentLocation) => _.sample(map[currentLocation]);
+const move = (map, location) => _.sample(map[location]);
 
-module.exports = { move };
+const works = (tasks, map) => {
+  let turn;
+  let currentLocation = randomLocation(map);
+  for (turn = 0; areTasksToWorkOn(tasks); turn++) {
+    updateTasksStage(tasks, currentLocation);
+    console.log(tasks);
+    const logFrom = `Move from ${currentLocation}`;
+    currentLocation = move(map, currentLocation);
+    console.log(`${logFrom} to ${currentLocation}`);
+  }
+  return `Done in ${turn} turns`;
+};
+
+module.exports = {
+  works,
+};
