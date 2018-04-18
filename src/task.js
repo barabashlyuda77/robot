ca058f069b3ce9de89c91dc0b141419755b2c5a2
lbarabash <barabashlyuda@gmail.com>
2018-04-18 15:47:35 +0200

    changed ui and renamed function

--- src/task.js
+++ src/task.js
@@ -1,20 +1,22 @@
 const _ = require('lodash');
 
 function getDestination(fromLocation, states) {
+const getDestination = (fromLocation, states) => {
   if (!states.includes(fromLocation)) throw new Error('A state is not on a map');
   if (states.length === 1) throw new Error('Not enough states');
 
   const destination = _.sample(states);
   return (fromLocation === destination) ? getDestination(fromLocation, states) : destination;
-}
+};
 
-function createToDoList(states, n) {
-  return _.range(n).map(() => {
-    const fromLocation = _.sample(states);
-    return {
-      from: fromLocation,
-      to: getDestination(fromLocation, states),
-    };
+const createTaskList = (map, n) => _.range(n).map(() => {
+  const fromLocation = randomLocation(map);
+  return {
+    from: fromLocation,
+    to: getDestination(fromLocation, getStates(map)),
+    stage: 'unstarted',
+  };
+});
   });
 }
 
